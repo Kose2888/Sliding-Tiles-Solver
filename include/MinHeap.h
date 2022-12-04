cc955f7376e9dd0999f2071a3847b238a381ed6a
Cam-Rose <114460670+Cam-Rose@users.noreply.github.com>
2022-12-04 14:26:33 -0700

    Create MinHeap.h
    This is a basic minHeap ADT

---
+++ include/MinHeap.h
@@ -0,0 +1,26 @@
+#ifndef MINHEAP_H
+#define MINHEAP_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#inlcude "Board_Tile.h"
+
+class MinHeap {
+ public:
+  MinHeap(const std::vector<Board_Tile> & initialItems);
+  
+  void insert(Board_Tile key);
+  
+  std::string deleteMin();
+  
+  bool isEmpty();
+  
+ private:
+  int size;
+  std::vector<Board_Tile> array;
+  
+  void perculateUp(int hole, int val);
+  void perculateDown(int hole, int val);
+};
+#endif //MINHEAP_H
